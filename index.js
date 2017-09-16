ca90592854d2b006e959e2e230c8cefa20449060
Cheikh Seck <cseck@bryant.edu>
2017-09-16 22:45:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
     $trukture.addPlugin({
      	name: "Goget-from-github",
      	show : function(code){
-     		return  ( !code.includes("\n") && ( code.includes("github.com") || code.split("/").length > 2 ) )
+               console.log(code);
+     		return  ( !code.includes("\n") &&  code.includes("github.com")  )
      	},
      	exec : function(code){
      			var formatstring = code.replace('"', '').replace('"', '').replace(/\s/g,'');
-     		 	$trukture.exec("go get " + formatstring,function(success){
+     		 	$trukture.exec("go get " + formatstring,function(success, log){
      		 		if (success !== false){
      		 			//	alert(formatstring + " fetched!")
      		 		} else {
-     		 			alert("Error running commmand!")
+     		 			alert("Error running commmand! " + log)
      		 		}
      		 	})
      		   return  code + " //fetched :)";
